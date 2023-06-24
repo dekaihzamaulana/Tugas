67fd9337b9363911da62c86692f95ec6d509d7cb
deka ihza maulana 1b <dekaihzamaulana@gmail.com>
2023-06-24 15:00:10 +0700

    TM 2

---
+++ src/tugas.java
@@ -0,0 +1,19 @@
+public class tugas {
+    public static void main(String[] args) {
+
+        for (int i=1; i<=100; i++){
+            System.out.println(i);
+            }
+        int nomor=1;
+        while (nomor <= 100) {
+            System.out.println(nomor);
+            nomor++;
+        }
+        int[] angka = {2,5,9,12,14};
+        for (int i=0; i<angka.length ; i++){
+            System.out.println(angka [i]);
+        }
+    }
+ }
+
+
