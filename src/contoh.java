be09f03096e5f40254c20bf162b487446cab1035
deka ihza maulana 1b <dekaihzamaulana@gmail.com>
2023-06-24 15:27:36 +0700

    TM 2

---
+++ src/contoh.java
@@ -0,0 +1,12 @@
+public class contoh {
+    public static void main(String[] args) {
+        printNumbers(2, 1000);
+    }
+
+    public static void printNumbers(int BatasAtas, int Batasbawah) {
+
+        for (int i = BatasAtas; i <= Batasbawah; i++) {
+            System.out.println(i);
+        }
+    }
+}
